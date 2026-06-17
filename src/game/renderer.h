81ee45843d01d8ea1be00b63f67374016ebb48d1
Jakob ferguson <jakob.b.ferguson@gmail.com>
2026-06-17 17:55:06 -0500

    headers

--- src/game/renderer.h
+++ src/game/renderer.h
@@ -1,11 +1,44 @@
-#include "../menu/menu.h"
 #pragma once
+
+#include <stdint.h>
+struct pixel_cell {
+    //length of the following glyph
+    uint8_t len;
+  
+    //glyph/character
+    char byte[4];   
+
+    //foreground rgb
+    uint8_t fg_r;
+    uint8_t fg_g;
+    uint8_t fg_b;
+
+    //background rgb
+    uint8_t bg_r;
+    uint8_t bg_g;
+    uint8_t bg_b;
+
+    //character flags bold, italic, etc
+    uint8_t attributes;
+};
 
 struct window {
     int height;
     int width;
+    struct pixel_cell *frame;
+    void *screen_buffer;
 };
 
-void renderer_clear_screen(void);
-void renderer_draw_border(struct window *w, char d);
-void renderer_renmenu(struct window *w, struct menu *m, int s);
+struct box {
+    int width;
+    int height;
+    uint8_t background_length;
+    int border_type;
+    char *background;
+};
+
+int init_window(int x, int y, struct window *w);
+int write_window(struct window *w);
+
+int init_box(struct box *b, int x, int y, char bg[], int border_type);
+int draw_box(struct box *b, struct window *w);
